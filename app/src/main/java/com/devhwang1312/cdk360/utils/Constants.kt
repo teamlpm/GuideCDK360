688751c8c4710084a7cc75b3774367f1076f0bb7
Vu Huong <huongvu1397@gmail.com>
2019-05-29 21:25:50 +0700

    huongv

--- app/src/main/java/com/devhwang1312/cdk360/utils/Constants.kt
+++ app/src/main/java/com/devhwang1312/cdk360/utils/Constants.kt
@@ -2,4 +2,8 @@ package com.ambe.filerecovery.utils
 
 object Constants {
     const val APP = "App"
+    const val CHAR_HOC_SY = 1
+    const val CHAR_THUONG_THUC = 2
+    const val CHAR_THUONG_DUOC = 3
+    const val CHAR_THI_VE = 4
 }
